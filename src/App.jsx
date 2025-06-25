6063d9d997554e227dd3874948ac1bb51e878c8c
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-06-25 23:01:31 +0700

    deploy1

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Content />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
